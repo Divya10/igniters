034386cc364bc7380a989eed19a132a023d1211a
divya10 <divya.agarwal24@outlook.com>
2018-03-30 10:20:11 +0530

    title bar and toggle button added

--- app/src/main/java/com/example/divya/igniters/MainActivity.java
+++ app/src/main/java/com/example/divya/igniters/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.divya.igniters;
 
+import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,60 +14,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout d;
-    //ActionBarDrawerToggle toggle;
+    //android.support.v7.app.ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        d=findViewById(R.id.d1);
-        Toolbar toolbar=findViewById(R.id.toolbar_main);
+        d = findViewById(R.id.d1);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
-        //d.addDrawerListener(toggle);
+        android.support.v7.app.ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp);
+//        getSupportActionBar().setHomeButtonEnabled(true);
         NavigationView navigationView = findViewById(R.id.nav_view);
         Fragment fragment1;
-        fragment1=new BlankFragment2();
+        fragment1 = new BlankFragment2();
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.f1, fragment1)
                 .commit();
         //toggle.syncState();
+//        toggle = new android.support.v7.app.ActionBarDrawerToggle(this, d,
+//                R.string.drawer_open, R.string.drawer_close) {
+//
+//            /** Called when a drawer has settled in a completely open state. */
+//            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+//                getSupportActionBar().setTitle("Navigation!");
+//                invalidateOptionsMenu();
+//            }
+//
+//            /** Called when a drawer has settled in a completely closed state. */
+//            public void onDrawerClosed(View view) {
+//                super.onDrawerClosed(view);
+//                getSupportActionBar().setTitle("Navigation!");
+//                invalidateOptionsMenu();
+//            }
+//
+//        };
+//        d.addDrawerListener(toggle);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 item.setChecked(true);
 
                 d.closeDrawers();
-                Fragment fragment=null;
+                Fragment fragment = null;
                 FragmentManager fragmentManager = getFragmentManager();
-                if(item.getTitle().toString().equalsIgnoreCase("Login"))
-                {
+                if (item.getTitle().toString().equalsIgnoreCase("Login")) {
 
 
+                } else if (item.getTitle().toString().equalsIgnoreCase("Signup")) {
 
-                }
-                else if(item.getTitle().toString().equalsIgnoreCase("Signup"))
-                {
+                } else if (item.getTitle().toString().equalsIgnoreCase("Nearby Hospital")) {
 
-                }
-                else if(item.getTitle().toString().equalsIgnoreCase("Nearby Hospital"))
-                {
-
-                }
-                else if(item.getTitle().toString().equalsIgnoreCase("About Dengue"))
-                {
-                    fragment=new AboutDengue();
-                }
-                else if(item.getTitle().toString().equalsIgnoreCase("Prevention"))
-                {
-                    fragment=new BlankFragment();
-                }
-                else
-                {
-                    fragment=new OurTeam();
+                } else if (item.getTitle().toString().equalsIgnoreCase("About Dengue")) {
+                    fragment = new AboutDengue();
+                } else if (item.getTitle().toString().equalsIgnoreCase("Prevention")) {
+                    fragment = new BlankFragment();
+                } else {
+                    fragment = new OurTeam();
                 }
                 fragmentManager.beginTransaction()
                         .replace(R.id.f1, fragment)
@@ -75,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onBackPressed() {
         if (d.isDrawerOpen(GravityCompat.START)) {
@@ -83,8 +98,26 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+
+//    @Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//        toggle.syncState();
+//    }
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//        toggle.onConfigurationChanged(newConfig);
+//    }
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+        case android.R.id.home:
+            d.openDrawer(GravityCompat.START);
+            return true;
     }
+    return super.onOptionsItemSelected(item);
+}
 
-
-
-
+}
